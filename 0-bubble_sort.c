fd94ce40ae4be44267c36a52e6e2d64a2c5212e3
root <root@DESKTOP-FPJE3Q0.localdomain>
2024-01-19 19:52:06 +0100

    0x1B. C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,23 +7,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t iNum = 0, iTmp = size;
-        int Saver;
+	size_t iNum = 0, iTmp = size;
+	int Saver;
 
-        if (array == NULL)
-                return;
+	if (array == NULL)
+		return;
 
-        for (iTmp = size; iTmp > 0; iTmp--)
-        {
-                for (iNum = 0; iNum < size - 1; iNum++)
-                {
-                        if (array[iNum] > array[iNum + 1])
-                        {
-                                Saver = array[iNum];
-                                array[iNum] = array[iNum + 1];
-                                array[iNum + 1] = Saver;
-                                print_array(array, size);
-                        }
-                }
-        }
+	for (iTmp = size; iTmp > 0; iTmp--)
+	{
+		for (iNum = 0; iNum < size - 1; iNum++)
+		{
+			if (array[iNum] > array[iNum + 1])
+			{
+				Saver = array[iNum];
+				array[iNum] = array[iNum + 1];
+				array[iNum + 1] = Saver;
+				print_array(array, size);
+			}
+		}
+	}
 }
