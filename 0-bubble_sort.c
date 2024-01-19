f5e25d57362adbe9200a03e22dc50a9e4db98741
root <root@DESKTOP-FPJE3Q0.localdomain>
2024-01-19 19:49:46 +0100

    0x1B. C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,28 +2,28 @@
 
 /**
  * bubble_sort - sort list with bubble
- * @Array: The array to be printed
- * @Size: Number of elements in @array
+ * @array: The array to be printed
+ * @size: Number of elements in @array
  */
-void bubble_sort(int *Array, size_t Size)
+void bubble_sort(int *array, size_t size)
 {
-	size_t iNum = 0, iTmp = Size;
-	int Saver;
+        size_t iNum = 0, iTmp = size;
+        int Saver;
 
-	if (Array == NULL)
-		return;
+        if (array == NULL)
+                return;
 
-	for (iTmp = Size; iTmp > 0; iTmp--)
-	{
-		for (iNum = 0; iNum < Size - 1; iNum++)
-		{
-			if (Array[iNum] > Array[iNum + 1])
-			{
-				Saver = Array[iNum];
-				Array[iNum] = Array[iNum + 1];
-				Array[iNum + 1] = Saver;
-				print_array(Array, Size);
-			}
-		}
-	}
+        for (iTmp = size; iTmp > 0; iTmp--)
+        {
+                for (iNum = 0; iNum < size - 1; iNum++)
+                {
+                        if (array[iNum] > array[iNum + 1])
+                        {
+                                Saver = array[iNum];
+                                array[iNum] = array[iNum + 1];
+                                array[iNum + 1] = Saver;
+                                print_array(array, size);
+                        }
+                }
+        }
 }
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,8 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - insertion sortting algorithm
+ * @list: linked list to sort
+*/
+
+void insertion_sort_list(listint_t **List)
