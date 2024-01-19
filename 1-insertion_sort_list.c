dcbb5b83c16d577dec86b8eadda6315d28399efe
root <root@DESKTOP-FPJE3Q0.localdomain>
2024-01-19 20:06:29 +0100

    0x1B. C - Sorting algorithms & Big O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,8 +1,37 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - insertion sortting algorithm
- * @list: linked list to sort
+* insertion_sort_list - insertion sorting algorithm
+* @list: linked list to sort
 */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *iTemp;
+	int n;
 
-void insertion_sort_list(listint_t **List)
+	if (!list)
+		return;
+
+	iTemp = *list;
+
+	while (iTemp)
+	{
+		while (iTemp)
+		{
+			if (iTemp->next)
+			{
+				if (iTemp->n > iTemp->next->n)
+				{
+					n = iTemp->n;
+					*(int *)&iTemp->n = iTemp->next->n;
+					*(int *)&iTemp->next->n = n;
+					iTemp = *list;
+					print_list(*list);
+					break;
+				}
+			}
+			
+			iTemp = iTemp->next;
+		}
+	}
+}
